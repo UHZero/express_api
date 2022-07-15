00af0d84d95a5e7065675c804a9f252eb18047ed
uhzero <aureo.moraes@gmail.com>
2022-07-15 02:43:30 -0300

    [config/utils] - mongoURI test

--- src/config/utils/getMongoUri.js
+++ src/config/utils/getMongoUri.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 function getMongoUri() {
   let uri = process.env.MONGO_URI || 'mongodb+srv://USERNAME:PASSWORD@cluster123.abc12.mongodb.net/app';
+  console.log(process.env.MONGO_URI);
   if (process.env.NODE_ENV === 'test') {
     uri = `${uri}_test`;
   }
---
+++ src/tests/config/utils/getMongoUri.spec.js
@@ -0,0 +1,9 @@
+const { getMongoUri } = require('../../../config/utils/getMongoUri');
+
+describe('[config/utils] - mongoURI test', () => {
+  it('should return the default environment variable', () => {
+    const mongoURI = getMongoUri();
+
+    expect(mongoURI).toBe(`${process.env.MONGO_URI}_test`);
+  });
+});
